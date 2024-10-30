d748904bbda9d0c78917281574084f4ff4934214
EstiEisen <estieisen@gmail.com>
2024-10-30 23:11:00 +0200

    implemnets controller functions

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,82 +4,64 @@ namespace App\Http\Controllers;
 
 use App\Models\Blog;
 use Illuminate\Http\Request;
+use App\Http\Requests\Blog\StoreUpdateBlogRequest;
 
 class BlogController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+          $blogs = Blog::all();
+          return view('blogs.index', compact('blogs'));
+        
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function store(StoreUpdateBlogRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        Blog::create([
+            'title' => $validatedData['title'],
+            'content' => $validatedData['content'],
+            'publication_date' => $validatedData['publication_date'],
+        ]);
+
+        return redirect()->route('blogs.index')->with('success', 'blog created successfully!');
+
+    }
+    public function update(StoreUpdateBlogRequest $request, $id)
+    {
+
+      $blog = Blog::find($id);
+      $blog->update($request->all());
+      return redirect()->route('blogs.index')
+        ->with('success', 'Blog updated successfully.');
+    }
+
+
+    public function destroy($id)
+    {
+      $blog = Blog::find($id);
+      $blog->delete();
+      return redirect()->route('blogs.index')
+        ->with('success', 'Blog deleted successfully');
+    }
+
     public function create()
     {
-        //
+      return view('blogs.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function show($id)
     {
-        //
+      $blog = Blog::find($id);
+      return view('blogs.show', compact('blog'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Blog  $blog
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Blog $blog)
+    public function edit($id)
     {
-        //
+      $blog = Blog::find($id);
+      return view('blogs.edit', compact('blog'));
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Blog  $blog
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Blog $blog)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Blog  $blog
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Blog $blog)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Blog  $blog
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Blog $blog)
-    {
-        //
-    }
+  
 }
